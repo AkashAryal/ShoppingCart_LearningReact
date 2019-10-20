a3c1d9e54a316cf84ae2a1127c42777c4f88b19a
aa753 <aa753@cornell.edu>
2019-10-20 16:48:32 -0400

    added totalCounter functionality to navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,6 +6,7 @@ export default class NavBar extends Component {
       <nav className="navbar navbar-light bg-light">
         <a className="navbar-brand" href="#">
           NavBar
+          <span className="badge badge-pill badge-secondary">{this.props.totalCounters}</span>
         </a>
       </nav>
     );
